8938fb4d7c247d6cd3e9f9ff9c85bd05b21fdf5c
JGalan <JGalan@LTKB051>
2017-02-17 13:17:25 +0100

    Inicio

--- WpfApplication1/DragDropManager.cs
+++ WpfApplication1/DragDropManager.cs
@@ -17,7 +17,8 @@ namespace WpfApplication1
                                                                                                                     typeof(DragDropManager),
                                                                                                                     new FrameworkPropertyMetadata(new PropertyChangedCallback(OnDropTargetAdvisorChanged)));
 
-        private static UIElement _droppingItem;
+        private static IDragSourceAdvisor _dragAdvisor;
+        private static IDropTargetAdvisor _dropAdvisor;
 
         public static void SetUseDragSourceAdvisor(DependencyObject d, IDragSourceAdvisor dragSource)
         {
@@ -39,7 +40,6 @@ namespace WpfApplication1
            return d.GetValue(DropTargetAdvisorProperty) as IDropTargetAdvisor;
         }
 
-
         #region OnDragSourceAdvisorChanged
         private static void OnDragSourceAdvisorChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
         {
@@ -50,8 +50,8 @@ namespace WpfApplication1
                 sourceElt.PreviewMouseMove += DragSource_PreviewMouseMove;
                 sourceElt.PreviewMouseLeftButtonUp += DragSource_PreviewMouseLeftButtonUp;
                 //Set the Drag source UI
-                IDragSourceAdvisor advisor = args.NewValue as IDragSourceAdvisor;
-                advisor.SourceUI = sourceElt;
+                //_dragAdvisor = args.NewValue as IDragSourceAdvisor;
+                //_dragAdvisor.SourceUI = sourceElt;
             }
             else if (args.NewValue == null && args.OldValue != null)
             {
@@ -66,27 +66,30 @@ namespace WpfApplication1
             IDragSourceAdvisor dragSourceAdvisor = GetUseDragSourceAdvisor(sender as DependencyObject);
         }
 
+        //If mouse move over the to drag item, then active move action.
         private static void DragSource_PreviewMouseMove(object sender, MouseEventArgs e)
         {
-            IDragSourceAdvisor dragSourceAdvisor = GetUseDragSourceAdvisor(sender as DependencyObject);
-
-            if (dragSourceAdvisor != null && dragSourceAdvisor.IsDraggable)
+            if (e.LeftButton == MouseButtonState.Pressed)
             {
-                IDataObject data = dragSourceAdvisor.GetDataObject();
-                DragDrop.DoDragDrop(sender as DependencyObject, data, DragDropEffects.Move);
+                var dragAdvisor = GetUseDragSourceAdvisor(sender as DependencyObject);
+
+                if (dragAdvisor.Equals(_dragAdvisor))
+                {
+                    IDataObject data = _dragAdvisor.GetDataObject();
+                    DragDrop.DoDragDrop(_dropAdvisor.TargetUI, data, DragDropEffects.Move);
+                }
             }
         }
 
+        //If selected item is draggable made it the to drag item
         private static void DragSource_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            IDragSourceAdvisor dragSourceAdvisor = GetUseDragSourceAdvisor(sender as DependencyObject);
+            var dragAdvisor = GetUseDragSourceAdvisor(sender as DependencyObject);
 
-            if (dragSourceAdvisor != null)
+            if (dragAdvisor != null && dragAdvisor.IsDraggable)
             {
-                if (dragSourceAdvisor.IsDraggable)
-                {
-                    _droppingItem = dragSourceAdvisor.SourceUI;
-                }
+                _dragAdvisor = dragAdvisor;
+                _dragAdvisor.SourceUI = sender as UIElement;
             }
         }
 
@@ -104,8 +107,8 @@ namespace WpfApplication1
                 targetElt.PreviewDrop += DropTarget_PreviewDrop;
                 targetElt.AllowDrop = true;
                 // Set the Drag source UI
-                IDropTargetAdvisor advisor = args.NewValue as IDropTargetAdvisor;
-                advisor.TargetUI = targetElt;
+                _dropAdvisor = args.NewValue as IDropTargetAdvisor;
+                _dropAdvisor.TargetUI = targetElt;
             }
             else if (args.NewValue == null && args.OldValue != null)
             {
@@ -134,7 +137,17 @@ namespace WpfApplication1
 
         private static void DropTarget_PreviewDragEnter(object sender, DragEventArgs e)
         {
-            IDropTargetAdvisor dragSource = GetUseDropTargetAdvisor(sender as DependencyObject);
+            var dropAdvisor = GetUseDropTargetAdvisor(sender as DependencyObject);
+
+            if (dropAdvisor.Equals(_dropAdvisor))
+            {
+                var data = e.Data;
+                if (dropAdvisor.IsValidDataObject(data))
+                {
+                    dropAdvisor.GetVisualFeedback(data);
+
+                }
+            }
         }
 
         #endregion
